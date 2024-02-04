9108ba3ae990a80cd5f37fd3eb568a10907fe634
Naved Pathan <navedpathan646@gmail.com>
2024-02-04 22:32:39 +0530

    update commit

--- server/index.js
+++ server/index.js
@@ -12,25 +12,25 @@ const app = express();
 dotenv.config();
 // for data crossing from front-end to backend (to cross the different server)
 
-let originUrl = process.env.ORIGIN_URL;
-app.use(cors(
-  {
-        origin: {originUrl},
-        // origin: "https://crud-react-js-client.vercel.app",
-        methods:["POST","GET","PUT","DELETE","*"],
-        credentials:true
-    }  
-  ));
+// let originUrl = process.env.ORIGIN_URL;
+// app.use(cors(
+//   {
+//         origin: {originUrl},
+//         // origin: "https://crud-react-js-client.vercel.app",
+//         methods:["POST","GET","PUT","DELETE","*"],
+//         credentials:true
+//     }  
+//   ));
 
-// app.use(cors());
+app.use(cors());
 
 // squence should be maintain
 
-// node js can't understand body recevied from frontend so we use body parser
-app.use(bodyParser.json({ extended: true }));
+// // node js can't understand body recevied from frontend so we use body parser
+// app.use(bodyParser.json({ extended: true }));
 
-// if any spaces leaves in url it convert into some code so it helps to decode it
-app.use(bodyParser.urlencoded({extended:true}));
+// // if any spaces leaves in url it convert into some code so it helps to decode it
+// app.use(bodyParser.urlencoded({extended:true}));
 
 // using different function on f=different route
 app.use('/',Router);
